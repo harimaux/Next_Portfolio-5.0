999e47d4af70345bac241252292c394bcd517691
harimaux <lukaszwitek20@googlemail.com>
2022-04-15 16:07:16 +0100

    Mobile button fixed

--- js/script.js
+++ js/script.js
@@ -61,6 +61,15 @@ document.onreadystatechange = () => {
     //Hides and show mobile menu
     mobileBtn.addEventListener("click", () => {
       nav.classList.toggle("navToggle");
+
+      //Applies animation on button when clicked
+      const menuLine1 = document.getElementById("mobileBtnLine1");
+      const menuLine2 = document.getElementById("mobileBtnLine2");
+      const menuLine3 = document.getElementById("mobileBtnLine3");
+
+      menuLine1.classList.toggle("mobileBtnLine1X");
+      menuLine2.classList.toggle("mobileBtnLine2X");
+      menuLine3.classList.toggle("mobileBtnLine3X");
     });
 
     //Top page
